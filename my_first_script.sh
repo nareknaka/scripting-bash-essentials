9e92702fdeb221fe58020cdb6e02cb228106d63a
nareknaka <grigoryan.narek.1993@gmail.com>
2025-05-21 20:36:24 +0400

    updating line 6

--- my_first_script.sh
+++ my_first_script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo  "your age"
+echo "your age"
 read age
-if [[$age -ge 0  ]] && [[$age -it 12]]: then 
-        echo "you are a child"
-    elif [[ $age -ge 12 ]] && [[ $age -lt 18 ]]; then
-        echo "you are a teenager"
-    elif [[ $age -ge 18 ]] && [[ $age -lt 60 ]]; then
-        echo "you are an adult"
-    else
-        echo "you are old "
-fi 
+if [[ $age -ge 0 ]] && [[ $age -lt 12 ]]; then
+    echo "you are a child"
+elif [[ $age -ge 12 ]] && [[ $age -lt 18 ]]; then
+    echo "you are a teenager"
+elif [[ $age -ge 18 ]] && [[ $age -lt 60 ]]; then
+    echo "you are an adult"
+else
+    echo "you are old"
+fi
