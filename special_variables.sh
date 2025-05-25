fc5fc6b1b92a3b220288f2aaa6d349543a76f03d
nareknaka <grigoryan.narek.1993@gmail.com>
2025-05-25 16:30:10 +0400

    add number of arguments

--- special_variables.sh
+++ special_variables.sh
@@ -1,37 +1,46 @@
-- using reserved variables
+# - using reserved variables
 
-Tasks:
+# Tasks:
 
-- create a script that does the following:
+# - create a script that does the following:
 
-prints the name of the script
-prints all arguments
-prints the number of arguments passed to the script
-prints 2nd and 4th argument
-print the exit code of -eq operation on 1st and 2nd arguments (use [[]])
-- execute your script like this: ./my_script.sh 1 2 hello world
+# prints the name of the script
+# prints all arguments
+# prints the number of arguments passed to the script
+# prints 2nd and 4th argument
+# print the exit code of -eq operation on 1st and 2nd arguments (use [[]])
+# - execute your script like this: ./my_script.sh 1 2 hello world
 
-- change the script arguments so that the result of the last operation (-eq) would be 0
+# - change the script arguments so that the result of the last operation (-eq) would be 0
 
-- run the script again with new arguments
+# - run the script again with new arguments
 
-Self-check:
+# Self-check:
 
-- first script run results:
+# - first script run results:
 
-./my_script.sh
+# ./my_script.sh
 
-1 2 hello world
+# 1 2 hello world
 
-4
+# 4
 
-2 world
+# 2 world
 
-1
+# 1
 
-- second script run results contain 0 as the last output
+# - second script run results contain 0 as the last output
 
-Helpful materials:
+# Helpful materials:
 
-https://tldp.org/LDP/abs/html/internalvariables.html
-https://devhints.io/bash
+# https://tldp.org/LDP/abs/html/internalvariables.html
+# https://devhints.io/bash
+
+#!/bin/bash
+
+# Print the name of the script
+echo "Acript name: $0"
+# Print all arguments
+echo "All arguments: $@"
+# Print the number of arguments passed to the script
+echo "Number of arguments: $#"
