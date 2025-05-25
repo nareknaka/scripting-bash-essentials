48cede693757b75924936869fd8b8d44a0bbdd31
nareknaka <grigoryan.narek.1993@gmail.com>
2025-05-25 17:51:30 +0400

    updating code

--- special_variables.sh
+++ special_variables.sh
@@ -38,21 +38,18 @@
 
 #!/bin/bash
 
-# Print the name of the script
 echo "Script name: $0"
-# Print all arguments
 echo "All arguments: $@"
-# Print the number of arguments passed to the script
 echo "Number of arguments: $#"
-# Print 2nd and 4th argument   
 echo "2nd argument: $2"
 echo "4th argument: $4"
-# Print the exit code of -eq operation on 1st and 2nd arguments
+
 if [[ $1 -eq $2 ]]; then
     echo "Exit code of -eq operation: 0"
 else
     echo "Exit code of -eq operation: 1"
 fi
+
 
 
 
